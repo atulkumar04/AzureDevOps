42972a87d29a094c93fac6b1cf35869427dd86f3
atul0401 <atul.emailme@gmail.com>
2020-08-24 23:28:57 +0530

    Add project files.

---
+++ DevOpsDemo.test/PostTestController.cs
@@ -0,0 +1,38 @@
+using DevOpsDemo.Controllers;
+using DevOpsDemo.Models;
+using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
+namespace DevOpsDemo.Test
+{
+    public class PostTestController
+    {
+        private PostRepository repository;
+        public PostTestController()
+        {
+            repository = new PostRepository();
+        }
+        [Fact]
+        public void Test_Index_View_Result()
+        {
+            //Arrange
+            var controller = new HomeController(this.repository);
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Test_Index_Return_Result()
+        {
+            //Arrange
+            var controller = new HomeController(this.repository);
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.NotNull(result);
+        }
+        
+    }
+}
